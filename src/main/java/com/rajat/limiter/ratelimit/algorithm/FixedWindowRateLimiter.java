9ae8cf4fc3ecfb61f0eee86778ea1ff65c2e53ae
i-rajatkandpal <115024147+i-rajatkandpal@users.noreply.github.com>
2026-01-30 14:39:58 +0530

    Implemented fixed window algorithm

--- src/main/java/com/rajat/limiter/ratelimit/algorithm/FixedWindowRateLimiter.java
+++ src/main/java/com/rajat/limiter/ratelimit/algorithm/FixedWindowRateLimiter.java
@@ -1,6 +1,5 @@
 package com.rajat.limiter.ratelimit.algorithm;
 
-import com.rajat.limiter.ratelimit.model.RateLimitAlgorithm;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import com.rajat.limiter.ratelimit.model.RateLimitResponse;
 import org.springframework.stereotype.Component;
@@ -18,18 +17,23 @@ public class FixedWindowRateLimiter implements RateLimiter {
 
     @Override
     public RateLimitResponse allow(String key, int limit, int windowSeconds){
+
+        //this rounds-down to the start of the current 60-second window
         long now = System.currentTimeMillis() / 1000;
         long windowStart = (now / windowSeconds) * windowSeconds;
 
+        // key can be IP, this is done so that different users get exclusive counters to limit api.
         String redisKey = "rate:fixed:" + key + ":" + windowStart;
 
-        long count = redisTemplate.opsForValue().increment(redisKey);
+        // counts how many request can be made by a particular user
+        Long count = redisTemplate.opsForValue().increment(redisKey);
 
-        if(count == 1){
+        //for first request start expiry
+        if (count == 1) {
             redisTemplate.expire(redisKey, Duration.ofSeconds(windowSeconds));
         }
 
-        boolean allowed = count != 0 && count <= limit;
+        boolean allowed = count <= limit;
         long remaining = Math.max(0, limit - count);
 
         return new RateLimitResponse(allowed, remaining, windowSeconds + windowStart);
