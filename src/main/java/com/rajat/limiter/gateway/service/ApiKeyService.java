c86bdb7a49295d54456db9717c185aeb410aa8aa
i-rajatkandpal <115024147+i-rajatkandpal@users.noreply.github.com>
2026-01-31 23:42:57 +0530

    temp for gateway

--- src/main/java/com/rajat/limiter/gateway/service/ApiKeyService.java
+++ src/main/java/com/rajat/limiter/gateway/service/ApiKeyService.java
@@ -1,4 +1,30 @@
 package com.rajat.limiter.gateway.service;
 
+import com.rajat.limiter.ratelimit.model.RateLimitAlgorithm;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
 public class ApiKeyService {
+    private final Map<String, String> apiKeys = new HashMap<>();
+    private final Map<String, PlanConfig> planConfigs = new HashMap<>();
+
+    public ApiKeyService(){
+        apiKeys.put("api-8148892552", "https://rajat-kandpal.com/data");
+        planConfigs.put("api-8148892552", new PlanConfig(10,60, RateLimitAlgorithm.TOKEN_BUCKET));
+    }
+
+    public boolean isValid(String apiKey){
+        return apiKeys.containsKey(apiKey);
+    }
+    public String getTargetUrl(String apiKey){
+        return apiKeys.get(apiKey);
+    }
+    public PlanConfig getPlanConfig(String apiKey){
+        return planConfigs.get(apiKey);
+    }
+
+
 }
--- src/main/java/com/rajat/limiter/gateway/service/PlanConfig.java
+++ src/main/java/com/rajat/limiter/gateway/service/PlanConfig.java
@@ -1,4 +1,28 @@
 package com.rajat.limiter.gateway.service;
 
+import com.rajat.limiter.ratelimit.algorithm.RateLimiter;
+import com.rajat.limiter.ratelimit.model.RateLimitAlgorithm;
+
 public class PlanConfig {
+    private final int limit;
+    private final int windowSeconds;
+    private final RateLimitAlgorithm rateLimitAlgorithm;
+
+    public PlanConfig(int limit, int windowSeconds, RateLimitAlgorithm rateLimitAlgorithm) {
+        this.limit = limit;
+        this.windowSeconds = windowSeconds;
+        this.rateLimitAlgorithm = rateLimitAlgorithm;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public int getWindowSeconds() {
+        return windowSeconds;
+    }
+
+    public RateLimitAlgorithm getRateLimitAlgorithm() {
+        return rateLimitAlgorithm;
+    }
 }
