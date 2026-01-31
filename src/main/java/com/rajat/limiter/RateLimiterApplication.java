3a1c85e4c6fee88524a4f8a548bef1427166063f
i-rajatkandpal <115024147+i-rajatkandpal@users.noreply.github.com>
2026-01-31 23:43:42 +0530

    Added RestTemplate bean

--- src/main/java/com/rajat/limiter/RateLimiterApplication.java
+++ src/main/java/com/rajat/limiter/RateLimiterApplication.java
@@ -3,7 +3,9 @@ package com.rajat.limiter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
@@ -11,6 +13,11 @@ public class RateLimiterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RateLimiterApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
