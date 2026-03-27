1473de04ec525d3e8c6c2b3d70c4a664e074bdb5
i-rajatkandpal <115024147+i-rajatkandpal@users.noreply.github.com>
2026-03-27 14:19:08 +0530

    removed

--- src/main/java/com/rajat/limiter/ratelimit/service/RateLimiterService.java
+++ src/main/java/com/rajat/limiter/ratelimit/service/RateLimiterService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 @Service
-public class RateLimiterService {
+public class  RateLimiterService {
 
     private final Map<RateLimitAlgorithm, RateLimiter> limiters;
 
